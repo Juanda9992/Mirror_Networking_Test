8f1f4ec6742ddb8ccab389d974f8243c747bbba9
Juanda999 <juanda999games@gmail.com>
2024-01-29 09:24:43 -0500

    Add basic movement to the palyer

--- Assets/Scripts/Player/Player_Movement.cs
+++ Assets/Scripts/Player/Player_Movement.cs
@@ -12,15 +12,26 @@ public class Player_Movement : MonoBehaviour
     [Header("Jumping Settings")]
     [SerializeField] private float jumpForce;
     [SerializeField] private KeyCode jumpKey = KeyCode.Space;
+    private bool canJump = false;
 
     void Update()
     {
         xAxis = Input.GetAxisRaw("Horizontal");
         zAxis = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetKeyDown(jumpKey))
+        {
+            canJump = true;
+        }
     }
 
     void FixedUpdate()
     {
-        rb.velocity = new Vector3(xAxis,rb.velocity.y,zAxis).normalized * moveSpeed;        
+        rb.velocity = new Vector3(xAxis* moveSpeed,rb.velocity.y,zAxis* moveSpeed) ;     
+        if(canJump)
+        {
+            canJump = false;
+            rb.velocity = new Vector3(rb.velocity.x,jumpForce,rb.velocity.z);
+        }   
     }
 }
