197019acf6c74607544d9705a9224bf96995daee
Juanda999 <juanda999games@gmail.com>
2024-02-07 14:17:43 -0500

    Add sound when the cannon shoots

--- Assets/Scripts/Enviroment/Canon_Behaviour.cs
+++ Assets/Scripts/Enviroment/Canon_Behaviour.cs
@@ -7,12 +7,12 @@ public class Canon_Behaviour : NetworkBehaviour
 {
     [SerializeField] private GameObject ballPrefab;
     [SerializeField] private float shootForce;
-
+    [SerializeField] private AudioSource thisSource;
     [ContextMenu("Shoot"),ClientRpc]
     public void Shoot()
     {
+        thisSource.Play();
         GameObject ball = Instantiate(ballPrefab,transform.position,Quaternion.identity);
-
         ball.GetComponent<Rigidbody>().velocity = transform.forward * shootForce;
     }
 }
