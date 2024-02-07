0844680945f4d4e7c3b96672cf382fa6a5b59477
Juanda999 <juanda999games@gmail.com>
2024-02-07 14:56:22 -0500

    Optimize the game for preventing spawn infinite balls

--- Assets/Scripts/Enviroment/Canon_Behaviour.cs
+++ Assets/Scripts/Enviroment/Canon_Behaviour.cs
@@ -8,11 +8,15 @@ public class Canon_Behaviour : NetworkBehaviour
     [SerializeField] private GameObject ballPrefab;
     [SerializeField] private float shootForce;
     [SerializeField] private AudioSource thisSource;
+    private GameObject currentBall;
     [ContextMenu("Shoot"),ClientRpc]
     public void Shoot()
     {
         thisSource.Play();
-        GameObject ball = Instantiate(ballPrefab,transform.position,Quaternion.identity);
-        ball.GetComponent<Rigidbody>().velocity = transform.forward * shootForce;
+        if(!currentBall)
+        {
+            currentBall = Instantiate(ballPrefab,transform.position,Quaternion.identity);
+        }
+        currentBall.GetComponent<Rigidbody>().velocity = transform.forward * shootForce;
     }
 }
