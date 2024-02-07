9a6c9b4c97c6d12953ebbbc4c9bb4c03c7625def
Juanda999 <juanda999games@gmail.com>
2024-02-07 14:38:09 -0500

    Add sound for the bridges

--- Assets/Scripts/Enviroment/Bridge_Behaviour.cs
+++ Assets/Scripts/Enviroment/Bridge_Behaviour.cs
@@ -7,6 +7,8 @@ public class Bridge_Behaviour : NetworkBehaviour
 {
     [SerializeField] private Renderer thisRender;
     [SerializeField] private BoxCollider thisCollider;
+    [SerializeField] private AudioSource thisAudio;
+    [SerializeField] private AudioClip disableAudio;
 
     [ContextMenu("TestBridge"),ClientRpc]
     public void DisableBridge()
@@ -16,6 +18,7 @@ public class Bridge_Behaviour : NetworkBehaviour
 
     private IEnumerator TweenBridge()
     {
+        thisAudio.Play();
         for(int i = 0; i < 3;i++)
         {
             Tween colorTween = thisRender.material.DOColor(Color.red,0.2f);
@@ -25,6 +28,7 @@ public class Bridge_Behaviour : NetworkBehaviour
         }
         thisCollider.enabled = false;
         thisRender.material.color = Color.red;
+        thisAudio.PlayOneShot(disableAudio);
 
         yield return new WaitForSeconds(1);
         thisCollider.enabled = true;
